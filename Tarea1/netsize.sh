cfff4e10be38e7c902fee853816475696ad0213f
Lisbeth Alvarado <94935452+Lisbeth02@users.noreply.github.com>
2021-12-11 20:24:16 -0500

    Update netsize.sh
    Procesos de rutas

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,1 +1,3 @@
 # Pasos para realizar ele ejercicio 1.10.3 parte 1
+1.- Para empezar se requiere utilizar el Git Bash debido a que es un programa de ùtil y tiene fácil uso. 
+2.- Después se busca y se la ruta:
