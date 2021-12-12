444dc25147d37e06ebed9b6cba9b54e4c92d2363
Lisbeth Alvarado <94935452+Lisbeth02@users.noreply.github.com>
2021-12-11 20:53:26 -0500

    Update netsize_all.sh
    Ejecutar código Git Bash

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -1,7 +1,11 @@
 # Tarea del ejercicio 1.10.3 Redes de plantas y polinizadores - Parte 2
-
-Esta parte se realizo en README.md
+:nerd_face:
+#Esta parte se realizo en README.md
 -  Se elige la misma ruta con el proceso anterior
    cd  OneDrive/CSB/unix/Saavedra2013
 - Se corre la información 
   cd Saavedra2013
+#En esta parte ejecutaremos el codigo de Git Bash 
+Utilizamos el for , do , done
+ for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
+ 
