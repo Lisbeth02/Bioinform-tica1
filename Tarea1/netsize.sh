722229c6fcff73cb184a6eed54b1ec8e4bbf25b2
Lisbeth Alvarado <94935452+Lisbeth02@users.noreply.github.com>
2021-12-11 20:33:07 -0500

    Update netsize.sh
    Escribir la ruta y el terminal

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,3 +1,8 @@
 # Pasos para realizar ele ejercicio 1.10.3 parte 1
 1.- Para empezar se requiere utilizar el Git Bash debido a que es un programa de ùtil y tiene fácil uso. 
-2.- Después se busca y se la ruta:
+2.- Después se busca y se ubica la ruta:
+   cd OneDrive/CSB/unix/Saavedra
+   
+# Seguido necesitamos ubicar el directorio y la ruta. 
+- En el terminal se escribe: 
+cat n1.txt
