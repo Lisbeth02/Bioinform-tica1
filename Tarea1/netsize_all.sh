bd65251573eac4c91c3589983091e33c7839203f
Lisbeth Alvarado <94935452+Lisbeth02@users.noreply.github.com>
2021-12-11 20:50:07 -0500

    Update netsize_all.sh
    Proceso de la misma ruta parte 2

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -1,3 +1,7 @@
-
-
 # Tarea del ejercicio 1.10.3 Redes de plantas y polinizadores - Parte 2
+
+Esta parte se realizo en README.md
+-  Se elige la misma ruta con el proceso anterior
+   cd  OneDrive/CSB/unix/Saavedra2013
+- Se corre la información 
+  cd Saavedra2013
